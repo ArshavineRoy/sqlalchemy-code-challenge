98d7dad890cde9c543999d22c0ec2248bd161a34
ArshavineRoy <arshavinewaema@gmail.com>
2023-09-03 13:24:33 +0300

    add Review model, proxy to Restaurant & Customer - create Review model with star_rating column - add created_at and updated_at columns in all models - Use association object models to establish a many-to-many relationship between the models by importing association_proxy from sqlalchemy.ext.associationproxy

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
+from sqlalchemy import ForeignKey, Column, Integer, String, MetaData, DateTime, func
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.ext.associationproxy import association_proxy
@@ -16,6 +16,12 @@ class Restaurant(Base):
     id = Column(Integer(), primary_key=True)
     name = Column(String(), index=True)
     price = (Integer())
+    created_at = Column(DateTime(), server_default=func.now())
+    updated_at = Column(DateTime(), onupdate=func.now())
+
+    reviews = relationship('Review', back_populates='restaurant', cascade='all, delete-orphan')
+    customers = association_proxy('reviews', 'customer',
+        creator=lambda cus: Review(customer=cus))
 
 
     def __repr__(self):
@@ -24,13 +30,18 @@ class Restaurant(Base):
             f'name={self.name}, ' + \
             f'price={self.price})'
 
-
 class Customer(Base):
     __tablename__ = 'customers'
 
     id = Column(Integer(), primary_key=True)
     first_name = (String())
     last_name = (String())
+    created_at = Column(DateTime(), server_default=func.now())
+    updated_at = Column(DateTime(), onupdate=func.now())
+
+    reviews = relationship('Review', back_populates='customer', cascade='all, delete-orphan')
+    restaurants = association_proxy('reviews', 'restaurant',
+        creator=lambda rs: Review(restaurant=rs))
 
     def __repr__(self):
 
@@ -38,11 +49,23 @@ class Customer(Base):
             f'first_name={self.first_name}, ' + \
             f'last_name={self.last_name})'
 
-    
+class Review(Base):
+    __tablename__ = 'reviews'
 
-# class Review(Base):
-#     # __tablename__ = 'reviews'
+    id = Column(Integer(), primary_key=True)
 
-#     # id = Column(Integer(), primary_key=True)
-#     pass
-    
+    star_rating = Column(Integer())
+    created_at = Column(DateTime(), server_default=func.now())
+    updated_at = Column(DateTime(), onupdate=func.now())
+
+    restaurant_id = Column(Integer(), ForeignKey('restaurants.id'))
+    customer_id = Column(Integer(), ForeignKey('customers.id'))
+
+    restaurant = relationship('Restaurant', back_populates='reviews')
+    customer = relationship('Customer', back_populates='reviews')
+
+    def __repr__(self):
+
+        return f'Review(id={self.id}, ' + \
+            f'star_rating={self.star_rating}, ' + \
+            f'restaurant_id={self.restaurant_id})'
