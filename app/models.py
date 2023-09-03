7a75f7d8c63bd49b805f9b68e0fbe535c1ee82e0
ArshavineRoy <arshavinewaema@gmail.com>
2023-09-03 12:14:01 +0300

    set up migrations & add basic imports to models.py

---
+++ app/models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.associationproxy import association_proxy
+
+convention = {
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+}
+metadata = MetaData(naming_convention=convention)
+
+Base = declarative_base(metadata=metadata)
